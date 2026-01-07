046d84bd6c8e9e169851ffbf686126df4aa5b30e
Redger Xu <redgerxu@gmail.com>
2026-01-07 13:55:17 -0800

    string builder + refactoring

--- src/prompt.rs
+++ src/prompt.rs
@@ -5,28 +5,26 @@ use crate::{
     vcs::section_git,
     venv::section_venv,
 };
+use string_builder::Builder;
 
 pub fn left() -> String {
     let venv_text = if CONFIG.venv_right_side {
         String::from("")
     } else {
-        let text = section_venv();
-        if text.is_empty() {
-            String::from("")
-        } else {
-            format!("{} ", section_venv())
-        }
+        section_venv()
     };
 
-    format!(
-        "{}{}{}{}{}{} ",
-        section_cwd(),
-        section_git(),
-        venv_text,
-        fg(CONFIG.color3),
-        CONFIG.icon_arrow,
-        RESET
-    )
+    let mut builder = Builder::default();
+
+    builder.append(section_cwd());
+    builder.append(section_git());
+    builder.append(venv_text.clone());
+    builder.append(fg(CONFIG.color3));
+    builder.append(CONFIG.icon_arrow);
+    builder.append(RESET);
+    builder.append(" ");
+
+    builder.string().unwrap()
 }
 
 pub fn right() -> String {
