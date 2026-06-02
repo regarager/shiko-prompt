ee77c50bfb2be39db286f028cb70425832f60f7f
Redger Xu <redgerxu@gmail.com>
2026-06-02 00:42:48 -0700

    moved shell init to binary, fixed default theme

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,27 @@ fn main() {
     let mode = args().nth(1).unwrap_or(String::from("--left"));
 
     match mode.as_str() {
-        "--left" => println!("{}", left()),
-        "--right" => println!("{}", right()),
+        "left" => println!("{}", left()),
+        "right" => println!("{}", right()),
+        "init" => {
+            println!(
+                r#"
+autoload -Uz vcs_info
+autoload -Uz add-zsh-hook
+
+build_prompt() {{
+  PROMPT=$(shiko left)
+  RPROMPT=$(shiko right)
+}}
+
+setopt prompt_subst
+
+add-zsh-hook precmd build_prompt
+add-zsh-hook chpwd build_prompt
+
+build_prompt
+        "#            )
+        }
         m => println!("unknown option {m}"),
     }
 }
