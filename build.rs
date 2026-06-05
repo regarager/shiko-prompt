4bb799e3bb9fa43636fb234347e61d59d2222c18
Redger Xu <redgerxu@gmail.com>
2026-06-04 17:46:05 -0700

    remove hotloading feature, only compile-time theme config

--- build.rs
+++ build.rs
@@ -8,27 +8,15 @@ fn main() {
 
     let canonical_path = config_file.canonicalize().expect("config file not found");
 
-    // hotloading
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = PathBuf::from(&out_dir).join("config.rs");
 
-    let mut content = String::new();
+    let config_content = fs::read_to_string(&canonical_path).expect("failed to read config file");
 
-    content.push_str("use crate::config_loader::ConfigSource;\n");
-
-    if env::var("SHIKO_HOTLOAD").is_ok() {
-        content.push_str(&format!(
-            "pub static CONFIG_SOURCE: ConfigSource = ConfigSource::Hotload(\"{}\");",
-            canonical_path.display()
-        ))
-    } else {
-        let config_content =
-            fs::read_to_string(&canonical_path).expect("failed to read config file");
-
-        content.push_str(&format!(
-                "pub static CONFIG_SOURCE: ConfigSource = ConfigSource::Hardcoded(r##\"{config_content}\"##);"
-            ))
-    }
+    let content = format!(
+        "pub static CONFIG_SOURCE: &str = r##\"{}\"##;",
+        config_content
+    );
 
     let _ = fs::write(&dest_path, content);
 
