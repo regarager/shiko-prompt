ed66be571f0351867f33a0cc377dff24fbe255a7
Redger Xu <redgerxu@gmail.com>
2026-06-07 15:59:35 -0700

    include zsh setup in binary

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use crate::prompt::{left, right};
+use crate::prompt::{print_left, print_right};
+use crate::zsh_init::zsh_init;
 use std::env::args;
 
 mod config;
@@ -6,32 +7,15 @@ mod config_loader;
 mod modules;
 mod prompt;
 mod utils;
+mod zsh_init;
 
 fn main() {
-    let mode = args().nth(1).unwrap_or(String::from("--left"));
+    let mode = args().nth(1).unwrap_or(String::from("left"));
 
     match mode.as_str() {
-        "left" => println!("{}", left()),
-        "right" => println!("{}", right()),
-        "init" => {
-            println!(
-                r#"
-autoload -Uz vcs_info
-autoload -Uz add-zsh-hook
-
-build_prompt() {{
-  PROMPT=$(shiko left)
-  RPROMPT=$(shiko right)
-}}
-
-setopt prompt_subst
-
-add-zsh-hook precmd build_prompt
-add-zsh-hook chpwd build_prompt
-
-build_prompt
-        "#            )
-        }
+        "left" => print_left(),
+        "right" => print_right(),
+        "init" => zsh_init(),
         m => println!("unknown option {m}"),
     }
 }
